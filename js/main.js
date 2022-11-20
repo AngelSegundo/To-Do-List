c2e491d1d4d4709c8d2126f26f13545269d33726
AngelSegundo <angel.segundo.dev@gmail.com>
2022-11-20 22:31:04 +0100

    Upgrade Actions

--- js/main.js
+++ js/main.js
@@ -1,19 +1,47 @@
 const form = document.querySelector("#form")
-const noteInput = document.querySelector("#noteInput")
+const noteInput = document.querySelector(".input")
 const addTask = document.querySelector("#addTask")
 const reset = document.querySelector("#reset")
 const taskList = document.querySelector("#taskList")
-const doneList = document.querySelector("#doneList")
 
-const taskArr = localStorage.getItem("notes") ? JSON.parse(localStorage.getItem("notes")) : []
+const taskArr = localStorage.getItem("note") ? JSON.parse(localStorage.getItem("note")) : []
 
 form.addEventListener("submit", e => {
     e.preventDefault()
     taskArr.push(noteInput.value)
-    localStorage.setItem("notes", JSON.stringify(taskArr))
-    // funcion de crear
+    localStorage.setItem("note", JSON.stringify(taskArr))
+    taskCreator(noteInput.value)
     noteInput.value = ""
 })
+
+const taskCreator = (str) => {
+    let task = document.createElement("li")
+    task.innerHTML =
+        `
+        <div class=" checkbox-con">
+                    <input id="checkbox" type="checkbox">
+                    ${str}
+                    <button class="btnEdit">
+                        <span class="btn-text-one-edit">Edit</span>
+                        <span class="btn-text-two-edit">Enable editing</span>
+                    </button>
+                    <button class="btnDelete" onclick="taskDelete(this.parentNode)">
+                        <span class="btn-text-one-delete">Delete</span>
+                        <span class="btn-text-two-delete">Are you Sure?</span>
+                    </button>
+        `
+    taskList.appendChild(task)
+}
+
+const taskDelete = (btn) => {
+    let taskToUpgrade = btn.parentNode
+    const index = taskArr.indexOf(taskToUpgrade)
+    taskArr.splice(index, 1)
+    localStorage.setItem("note", JSON.stringify(taskArr))
+    taskToUpgrade.remove()
+}
+
+
 
 
 
